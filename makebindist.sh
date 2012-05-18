381a71b4f2a4dd93fe548777d09b142b975ba94e
Nick Shrine <nshrine@users.noreply.github.com>
2012-05-18 15:55:15 +0000

    Fixed makebindist for windows zip files

--- makebindist.sh
+++ makebindist.sh
@@ -2,15 +2,27 @@
 
 VERSION=`awk '$2 == "VERSION" { gsub("\"", "", $3); print $3 }' config.h`
 
-function makebindist {
+makebindist()
+{
 	HOST=$1
 	ARCH=$2
 	BITS=$3
 	EXTRA=$4
-	./configure --host=x86_64-linux-gnu CFLAGS=-m$BITS --prefix=/tmp/$ARCH $EXTRA &&
+	./configure --host=$HOST CFLAGS=-m$BITS --prefix=/tmp/$ARCH $EXTRA &&
 	make clean && make install &&
 	tar -zcvf bint-$VERSION-$ARCH.tar.gz -C /tmp/$ARCH . &&
 	rm -rf /tmp/$ARCH
+}
+
+makewinzip()
+{
+	BASE=$1
+	mkdir /tmp/win
+	tar -zxvf ${BASE}.tar.gz -C /tmp/win
+	rm -f ${BASE}.tar.gz
+	[ -f ${BASE}.zip ] && rm -f ${BASE}.zip
+	zip -b /tmp/win/bin/ ${BASE}.zip *.exe
+	rm -rf /tmp/win
 }
 
 # Linux
@@ -22,5 +34,7 @@ makebindist i686-apple-darwin10 Darwin_x86_64 64 ac_cv_func_malloc_0_nonnull=yes
 makebindist i686-apple-darwin10 Darwin_i386 32 ac_cv_func_malloc_0_nonnull=yes
 
 # Windows
-makebindist x86_64-w64-mingw32 win_x86_64 64 ac_cv_func_malloc_0_nonnull=yes
-makebindist i686-w64-mingw32 win_i386 32 ac_cv_func_malloc_0_nonnull=yes
+makebindist x86_64-w64-mingw32 win_x86_64 64 ac_cv_func_malloc_0_nonnull=yes &&
+makewinzip bint-$VERSION-win_x86_64
+makebindist i686-w64-mingw32 win_i386 32 ac_cv_func_malloc_0_nonnull=yes &&
+makewinzip bint-$VERSION-win_i386
