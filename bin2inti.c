838714812c8020bd9025d4ec5b7fafc70bb65309
Nick Shrine <nshrine@users.noreply.github.com>
2012-08-17 10:00:19 +0000

    Working text index version

--- bin2inti.c
+++ bin2inti.c
@@ -11,18 +11,22 @@
 #include <error.h>
 #include <errno.h>
 
+const int base = 10;
+
 int main(int argc, char *argv[])
 {
 	if (argc < 3) {
-		fprintf(stderr, "Usage: %s BINFILE IDSFILE\n", argv[0]);
-		exit(1);
+		fprintf(stderr, "Usage: %s BINFILE IDXFILE\n", argv[0]);
+		exit(EXIT_FAILURE);
 	}
 
 	char *binfile = argv[1];
 	char *idxfile = argv[2];
-	FILE *binfp = fopen(binfile, "r");
-	if (!binfp)
+
+	int binfd = open(binfile, O_RDONLY);
+	if (binfd == -1)
 		error(1, errno, "%s", binfile);
+	
 	FILE *idxfp;
 	if (strcmp(idxfile, "-") == 0)
 		idxfp = stdin;
@@ -32,15 +36,16 @@ int main(int argc, char *argv[])
 		error(1, errno, "%s", idxfile);
 	
 	struct stat buf;
-	if (stat(binfile, &buf) == -1)
+	if (fstat(binfd, &buf) == -1)
 		error(1, errno, "%s", binfile);
 	off_t size = buf.st_size;
-	float *map = mmap(0, size, PROT_READ, MAP_SHARED, fileno(binfp), 0);
+	
+	float *map = mmap(0, size, PROT_READ, MAP_SHARED, binfd, 0);
 	if (map == MAP_FAILED) {
 		error(1, errno, "Unable to memory map %s", binfile);
     }
 
-	char *line = NULL, *word, *p;
+	char *line = NULL, *word, *p, *endptr;
 	size_t n, nvals, i;
 
 	getline(&line, &n, idxfp);
@@ -55,11 +60,15 @@ int main(int argc, char *argv[])
 		p = strchr(line, '\n');
 		if (p)
 			*p = '\0';
+
 		i = 0;
 		word = strtok(line, " ");
-		idx[i++] = atol(word);
-		while ((word = strtok(NULL, " ")) != NULL)
-			idx[i++] = atol(word);
+		do {
+			idx[i++] = strtol(word, &endptr, base);
+			if (*endptr != '\0')
+				error(1, errno, "Invalid index: %s", word);
+		} while ((word = strtok(NULL, " ")) != NULL);
+
 		for (i = 0; i < nvals; i++)
 			printf("%f\n", map[idx[i]]);
 	}
@@ -67,8 +76,9 @@ int main(int argc, char *argv[])
 	free(line);
 	if (munmap(map, size) == -1)
 		error(1, errno, "%s", "unmapping file");
-	fclose(idxfp);
-	fclose(binfp);
+	close(binfd);
+	if (idxfp != stdin)
+		fclose(idxfp);
 
 	exit(EXIT_SUCCESS);
 }
